24a570c9c9b4c36d296413c5af192c095b00aae1
Erick Magario <erick.magario.barretto@gmail.com>
2025-09-30 10:53:26 -0300

    first commit

---
+++ funcoes.py
@@ -0,0 +1,31 @@
+import pygame
+
+def inicializa():
+
+    pygame.init()
+
+    window = pygame.display.set_mode((320, 240))
+
+    pygame.display.set_caption('Jogo do Erick')
+
+    return window 
+ 
+def recebe_eventos():
+
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            return False
+    return True 
+                               
+def desenha (window):
+    window.fill((255, 0, 0))
+
+    pygame.display.update() 
+    
+def game_loop(window):
+
+    while True:
+        if recebe_eventos() == False:
+            break
+        desenha(window) 
+
